72319e588b6dcb7a412247af378c13da1abb75b4
songwenming <swmyjx@sina.com>
2019-11-02 17:28:08 +0800

    mybatise 自动生成

---
+++ generator/src/com/mmall/model/SysUser.java
@@ -0,0 +1,95 @@
+package com.mmall.model;
+
+import java.util.Date;
+
+public class SysUser {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String display;
+
+    private String status;
+
+    private String contact;
+
+    private String memo;
+
+    private String deleted;
+
+    private Date deletedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display == null ? null : display.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact == null ? null : contact.trim();
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo == null ? null : memo.trim();
+    }
+
+    public String getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(String deleted) {
+        this.deleted = deleted == null ? null : deleted.trim();
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
